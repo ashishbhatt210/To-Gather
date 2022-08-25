ab7eac589bfeaafcab88a7d9dfde94e5e9b79699
ritirawat <ritirawat@egencia.com>
2022-08-25 21:25:16 +0530

    second

--- src/components/Aqimap.js
+++ src/components/Aqimap.js
@@ -81,14 +81,30 @@ const Aquimap = () => {
                             }}
                         >
                             <div className="container">
-                                <div className="item">1</div>
-                                <div className="item">2</div>
-                                <div className="item">3</div>
-                                <div className="item">4</div>
-                                <div className="item">5</div>
-                                <div className="item">6</div>
-                                <div className="item">7</div>
-                                <div className="item">8</div>
+                                <div className="item-1">
+                                    <h3 className={"item_name"}>Co = {countryData.co}</h3>
+                                </div>
+                                <div className="item-2">
+                                    <h3>Co = {countryData.co}</h3>
+                                </div>
+                                <div className="item-2">
+                                    <h3>Co = {countryData.co}</h3>
+                                </div>
+                                <div className="item-1">
+                                    <h3>Co = {countryData.co}</h3>
+                                </div>
+                                <div className="item-1">
+                                    <h3>Co = {countryData.co}</h3>
+                                </div>
+                                <div className="item-2">
+                                    <h3>Co = {countryData.co}</h3>
+                                </div>
+                                <div className="item-2">
+                                    <h3>Co = {countryData.co}</h3>
+                                </div>
+                                <div className="item-1">
+                                    <h3>Co = {countryData.co}</h3>
+                                </div>
                             </div>
                         </Popup>
                     ) : null
