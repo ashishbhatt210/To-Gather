4b9c5914f4f3d54a0ea7374fa63ae100ae3101e2
ritirawat <ritirawat@egencia.com>
2022-08-27 02:24:58 +0530

    chat

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,200 +1,138 @@
 import React, { useState, useEffect } from "react";
-import Marquee from "react-fast-marquee";
 import "./ChatRoom.css"
+import userData from "../data/user.json"
+import usermessages from "../data/eventchats.json"
+import users from "../data/user.json"
+import button from "bootstrap/js/src/button";
 const ChatRoom = () => {
+    const [messagesList, setmessagesList] = useState([]);
+    const [usersList, setUserList] = useState(users.users);
+    console.log(usersList);
+    const [messaging, setMessaging] = useState({
+        "authorId": "",
+        "text": "",
+        "time": ""
+    });
+
+    const currentMessageId="0";
+const currentUserId = 0;
+
+const postMessage =(event)=>{
+    const text=event.target.value;
+    var timestamp = new Date().getTime();
+    setMessaging({"authorId": currentUserId.toString(),"text":text.toString(),"time":timestamp.toString()});
+};
+    const sendMessage = async (event) => {
+        event.preventDefault();
+        const {authorId,text,time}=messaging
+        var timestamp = new Date().getTime();
+        const nextItem=messagesList.length;
+        await fetch(`https://climex-5bd2f-default-rtdb.firebaseio.com/messages/chats/${currentMessageId}/messaging/${nextItem}.json`, {
+            method: 'PUT',
+            body: JSON.stringify({
+             authorId,
+              text,
+                time
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        }).catch((err) => {
+                console.log(err.message);
+            });
+        setMessaging({"authorId": currentUserId.toString(),"text":"","time":timestamp.toString()});
+
+    };
+
+// const eventmessages = usermessages.chats[parseInt(currentMessageId)].messaging
+
+    useEffect(() => {
+
+        fetch('https://climex-5bd2f-default-rtdb.firebaseio.com/messages.json')
+            .then(results => results.json())
+            .then(data => {
+                setmessagesList(data.chats[parseInt(currentMessageId)].messaging);
+                console.log(messagesList);
+
+
+            })
+
+    }, [setMessaging]);
 
 
     return (
+
+
         <div id="container">
             <aside>
                 <header>
                     <input type="text" placeholder="search"/>
                 </header>
-                <ul>
-                    <li>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_01.jpg" alt=""/>
+                <ul>{
+                    userData.users.map(user => (
+                        user.id !== currentUserId ?
+
+                            (<li key={user.id}>
+                            <img src={user.image} alt=""/>
+
                             <div>
-                                <h2>Prénom Nom</h2>
+                                <h2>{user.name}</h2>
                                 <h3>
                                     <span className="status orange"></span>
-                                    offline
+                                    {user.status}
                                 </h3>
                             </div>
-                    </li>
-                    <li>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_02.jpg" alt=""/>
-                            <div>
-                                <h2>Prénom Nom</h2>
-                                <h3>
-                                    <span className="status green"></span>
-                                    online
-                                </h3>
-                            </div>
-                    </li>
-                    <li>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_03.jpg" alt=""/>
-                            <div>
-                                <h2>Prénom Nom</h2>
-                                <h3>
-                                    <span className="status orange"></span>
-                                    offline
-                                </h3>
-                            </div>
-                    </li>
-                    <li>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_04.jpg" alt=""/>
-                            <div>
-                                <h2>Prénom Nom</h2>
-                                <h3>
-                                    <span className="status green"></span>
-                                    online
-                                </h3>
-                            </div>
-                    </li>
-                    <li>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_05.jpg" alt=""/>
-                            <div>
-                                <h2>Prénom Nom</h2>
-                                <h3>
-                                    <span className="status orange"></span>
-                                    offline
-                                </h3>
-                            </div>
-                    </li>
-                    <li>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_06.jpg" alt=""/>
-                            <div>
-                                <h2>Prénom Nom</h2>
-                                <h3>
-                                    <span className="status green"></span>
-                                    online
-                                </h3>
-                            </div>
-                    </li>
-                    <li>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_07.jpg" alt=""/>
-                            <div>
-                                <h2>Prénom Nom</h2>
-                                <h3>
-                                    <span className="status green"></span>
-                                    online
-                                </h3>
-                            </div>
-                    </li>
-                    <li>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_08.jpg" alt=""/>
-                            <div>
-                                <h2>Prénom Nom</h2>
-                                <h3>
-                                    <span className="status green"></span>
-                                    online
-                                </h3>
-                            </div>
-                    </li>
-                    <li>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_09.jpg" alt=""/>
-                            <div>
-                                <h2>Prénom Nom</h2>
-                                <h3>
-                                    <span className="status green"></span>
-                                    online
-                                </h3>
-                            </div>
-                    </li>
-                    <li>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_10.jpg" alt=""/>
-                            <div>
-                                <h2>Prénom Nom</h2>
-                                <h3>
-                                    <span className="status orange"></span>
-                                    offline
-                                </h3>
-                            </div>
-                    </li>
+
+                        </li>):null
+
+                    ))
+                }
                 </ul>
             </aside>
             <main>
                 <header>
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_01.jpg" alt=""/>
+                    <img src={userData.users[parseInt(currentUserId)].image} alt=""/>
                         <div>
-                            <h2>Chat with Vincent Porter</h2>
+                            <h2>{userData.users[parseInt(currentUserId)].name}</h2>
                             <h3>already 1902 messages</h3>
                         </div>
                         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_star.png" alt=""/>
                 </header>
                 <ul id="chat">
-                    <li className="you">
-                        <div className="entete">
-                            <span className="status green"></span>
-                            <h2>Vincent</h2>
-                            <h3>10:12AM, Today</h3>
-                        </div>
-                        <div className="triangle"></div>
-                        <div className="message">
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                        </div>
-                    </li>
-                    <li className="me">
-                        <div className="entete">
-                            <h3>10:12AM, Today</h3>
-                            <h2>Vincent</h2>
-                            <span className="status blue"></span>
-                        </div>
-                        <div className="triangle"></div>
-                        <div className="message">
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                        </div>
-                    </li>
-                    <li className="me">
-                        <div className="entete">
-                            <h3>10:12AM, Today</h3>
-                            <h2>Vincent</h2>
-                            <span className="status blue"></span>
-                        </div>
-                        <div className="triangle"></div>
-                        <div className="message">
-                            OK
-                        </div>
-                    </li>
-                    <li className="you">
-                        <div className="entete">
-                            <span className="status green"></span>
-                            <h2>Vincent</h2>
-                            <h3>10:12AM, Today</h3>
-                        </div>
-                        <div className="triangle"></div>
-                        <div className="message">
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                        </div>
-                    </li>
-                    <li className="me">
-                        <div className="entete">
-                            <h3>10:12AM, Today</h3>
-                            <h2>Vincent</h2>
-                            <span className="status blue"></span>
-                        </div>
-                        <div className="triangle"></div>
-                        <div className="message">
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                        </div>
-                    </li>
-                    <li className="me">
-                        <div className="entete">
-                            <h3>10:12AM, Today</h3>
-                            <h2>Vincent</h2>
-                            <span className="status blue"></span>
-                        </div>
-                        <div className="triangle"></div>
-                        <div className="message">
-                            OK
-                        </div>
-                    </li>
+                    {
+                        messagesList.map(chats => (
+                            chats.authorId !== currentUserId.toString() ?
+                                (<li className="you">
+                             <div className="entete">
+                                <span className="status green"></span>
+                                <h2>{usersList[parseInt(chats.authorId)].name}</h2>
+                                <h3>10:12AM, Today</h3>
+                            </div>
+                            <div className="triangle"></div>
+                            <div className="message">
+                                {chats.text}
+                            </div>
+                        </li>):(<li className="me">
+                                    <div className="entete">
+                                        <span className="status green"></span>
+                                        <h2>{usersList[parseInt(chats.authorId)].name}</h2>
+                                        <h3>10:12AM, Today</h3>
+                                    </div>
+                                    <div className="triangle"></div>
+                                    <div className="message">
+                                        {chats.text}
+                                    </div>
+                                </li>)
+                        ))
+                    }
                 </ul>
-                <footer>
-                    <textarea placeholder="Type your message"></textarea>
+                <footer className="align-items-center d-flex flex-column">
+
+                    <textarea placeholder="Type your message" value={messaging.text} onChange={postMessage}></textarea>
+                    <div className="footer_class">
                     <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_picture.png" alt=""/>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_file.png" alt=""/>
-                            <a href="#">Send</a>
+                    <button className="button_send" onClick={sendMessage} role="button">Send</button>
+                    </div>
                 </footer>
             </main>
         </div>
