00291bec93bb90341c91e3db2a22401d7b09d502
Ashish Bhatt <bhattashish210@gmail.com>
2022-08-26 20:46:23 +0530

    navbar-update

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,11 @@ import '../App.css';
 import Aquimap from './Aqimap';
 import CardApp from './Cards'
 import Label from "./Logo"
+import Navbar from './navbar';
 function Home() {
     return (
         <div className={"outer_container_big"}>
+                        <Navbar/>
                         <Label/>
                         <Aquimap/>
                         <CardApp/>
