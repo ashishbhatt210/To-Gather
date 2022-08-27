e73a4fc933bd9a2e41f656dc973356e5bd7dfd87
ritirawat <ritirawat@egencia.com>
2022-08-27 12:10:41 +0530

    map

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,23 +5,23 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top ">
             <div className="container-fluid">
-                <span className="navbar-brand" href="#">Climex</span>
+                <span className="navbar-brand mx-4"  href="#">Climex</span>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        <li className="nav-item mx-2">
+                            <a className="nav-link " aria-current="page" href="/">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/chatroom">Discussion</a>
+                            <a className="nav-link " href="/chatroom">Discussion</a>
                         </li>
-                        <li className="nav-item dropdown">
+                        <li className="nav-item dropdown mx-2">
                             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Dropdown
                             </a>
-                            <ul className="dropdown-menu">
+                            <ul className="dropdown-menu mx-2">
                                 <li><a className="dropdown-item" href="#">Action</a></li>
                                 <li><a className="dropdown-item" href="#">Another action</a></li>
                                 <li><hr className="dropdown-divider"></hr></li>
