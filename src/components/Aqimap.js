c5e1e6c197d7ab3c81fa197d033fac76140bc0b2
ritirawat <ritirawat@egencia.com>
2022-08-27 16:59:39 +0530

    map done

--- src/components/Aqimap.js
+++ src/components/Aqimap.js
@@ -4,11 +4,13 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import "../App.css"
 import "./Aquimap.css"
 import  countries from "../data/latitues_longitudes.json"
-import danger from "../assets/danger.png";
+import mark from "../assets/mark.png";
 
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiZ29kZmxhbWUiLCJhIjoiY2w3ODN5bXI2MDV6bDNybzN1N21hZnJyaSJ9.kNtArpczBGeDcqGNZCk2yQ'; // Set your mapbox token here
 const baseURL = 'http://api.openweathermap.org/data/2.5/air_pollution?';
+const baseURL2 = 'http://api.openweathermap.org/geo/1.0/direct?';
+
 const apiKey = '89179d92f2cf39ffcbd592fe378db8ff';
 
 const Aquimap = () => {
@@ -17,18 +19,63 @@ const Aquimap = () => {
         "longitude":0.0
     });
 
+    const[city,setCity]=useState("");
+    const[state,setState]=useState("");
+    const[country,setCountry]=useState("");
+    const[backgroundImg,setBackgroundImg]=useState("");
+    const[countryList,setCountryList]=useState(countries.country)
     const [countryData, setCountryData] = useState(null);
+    const[points,setPoints]=useState({
+        "latitude": 29,
+        "longitude": 76.45,
+        "zoom": 8}
+    );
 
-    // {
-    //     "co": 203.609,
-    //     "no": 0.0,
-    //     "no2": 0.396,
-    //     "o3": 75.102,
-    //     "so2": 0.648,
-    //     "pm2_5": 23.253,
-    //     "pm10": 92.214,
-    //     "nh3": 0.117
-    // }
+    let name,value;
+    const changeDetail =(event)=>
+    {
+        name=event.target.name;
+value=event.target.value;
+if(name === "state")
+{
+ setState(value);
+}
+else if(name === "city"){
+    setCity(value);
+}
+else {
+    setCountry(value);
+}}
+    const putOnMap=(event)=>{
+        // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+            fetch(`${baseURL2}q=${city},${state},${country}&limit=1&appid=${apiKey}`)
+                .then(results => results.json())
+                .then(data => {
+
+                    setPoints({
+                        "latitude": data[0].lat,
+                        "longitude": data[0].lon,
+                        "zoom": 10
+                    })
+                })
+
+        countries.country.push({"latitude": points.latitude,
+            "longitude": points.longitude});
+            setCountryList(countries.country);
+
+
+        fetch(`${baseURL}lat=${points.latitude}&lon=${points.longitude}&&appid=${apiKey}`)
+            .then(results => results.json())
+            .then(data => {
+                const pollutant = data.list[0].components;
+                if(pollutant.no2>=200||pollutant.pm10>=90||pollutant.o3>=180)
+                    setBackgroundImg("DANGER")
+                else
+                    setBackgroundImg("SAFE")
+            })
+    }
+
+
 
     useEffect(() => {
 
@@ -38,20 +85,16 @@ const Aquimap = () => {
     return (
         <div  className={"map_container d-flex flex-row  "} >
             <Map
-                initialViewState={{
-                    latitude: 28.72,
-                    longitude: 77.45,
-                    zoom: 8}
-
-                }
+                {...points}
+                onMove={evt => setPoints(evt.viewState)}
                 style={{width: "51%", height: "600px",marginLeft: "6%",borderRadius:"20px" ,boxShadow:"14px 14px 20px #000000, -14px -14px 20px #000000"}}
                 mapStyle="mapbox://styles/mapbox/streets-v9"
-
                 mapboxAccessToken={MAPBOX_TOKEN}
             >
                 {
-                    countries.country.map(country => (
+                    countryList.map(country => (
                     <Marker
+                        key={country.latitude+country.longitude}
                         style={{width: "50px", height: "50px"}}
                         longitude={country.longitude} latitude={country.latitude}>
                         <button
@@ -62,15 +105,21 @@ const Aquimap = () => {
                                     .then(results => results.json())
                                     .then(data => {
                                         const pollutant = data.list[0].components;
+
                                         setCountryData(pollutant)
                                         setCoordinate(country)
                                     })
                             }}>
-                            <img src={danger} alt="Skate Park Icon" width="50"/>
-                        </button>
+
+
+                                <img src={mark} alt="Skate Park Icon" width="50"/>
+
+                             </button>
                     </Marker>
-                    ))}
+                    ))
+                }
                 {
+
                     countryData ? (
                         <Popup
                             latitude={coordinate.latitude}
@@ -81,12 +130,14 @@ const Aquimap = () => {
 
                             }}
                         >
+
                             <div className="container-popup">
                                 <div className="item-1">
                                     <h3 className={"item_name"}>Co = {countryData.co}</h3>
                                 </div>
                                 <div className="item-2">
                                     <h3 className={"item_name"}>No = {countryData.no}</h3>
+
                                 </div>
                                 <div className="item-2">
                                     <h3 className={"item_name"}>NO2 = {countryData.no2}</h3>
@@ -98,10 +149,10 @@ const Aquimap = () => {
                                     <h3 className={"item_name"}>SO2 = {countryData.so2}</h3>
                                 </div>
                                 <div className="item-2">
-                                    <h3 className={"item_name"}>PM2_5 = {countryData.pm2_5}</h3>
+                                    <h3 className={"item_name"}>PM25 = {countryData.pm2_5}</h3>
                                 </div>
                                 <div className="item-2">
-                                    <h3 className={"item_name"}>PM_10 = {countryData.pm_10}</h3>
+                                    <h3 className={"item_name"}>PM10 = {countryData.pm_10}</h3>
                                 </div>
                                 <div className="item-1">
                                     <h3 className={"item_name"}>NH3 = {countryData.nh3}</h3>
@@ -119,12 +170,13 @@ const Aquimap = () => {
                 <div className="brand-title">Search Any Location </div>
                 <div className="inputs-form">
                     <label className={"label-form font-weight-bold"}>CITY *</label>
-                    <input className={" input-form "} type="text" placeholder="City"/>
+                    <input className={" input-form "} type="text" placeholder="City" value={city} onChange={changeDetail} name="city"/>
                     <label className={"label-form font-weight-bold"}>STATE</label>
-                    <input className={" input-form "} type="text" placeholder="State"/>
+                    <input className={" input-form "} type="text" placeholder="State" value={state} onChange={changeDetail} name="state"/>
                     <label className={"label-form font-weight-bold"}>COUNTRY *</label>
-                    <input className={" input-form "} type="text" placeholder="Country"/>
-                    <button className={"button-form button-form mt-5"} type="submit">SEARCH</button>
+                    <input className={" input-form "} type="text" placeholder="Country" value={country} onChange={changeDetail} name="country"/>
+                    <button className={"button-form button-form mt-5"} type="submit" onClick={putOnMap}>SEARCH</button>
+                    <div className="brand-title-again align-self-center">{backgroundImg}</div>
                 </div>
             </div>
 
