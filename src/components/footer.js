7becba241c566b49216878be29a401e57b582fc8
ritirawat <ritirawat@egencia.com>
2022-08-28 07:44:52 +0530

    added final touch

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <div class="row">
           <div class="col-sm-12 col-md-6">
             <h6>About</h6>
-            <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+            <p class="text-justify"> TO-GATHER  WANTS TO BE SIMPLE is an initiative  to help the upcoming programmers with the code.  TO-GATHER focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
           </div>
 
           <div class="col-xs-6 col-md-3">
@@ -40,11 +40,11 @@ const Footer = () => {
         <div class="row">
           <div class="col-md-8 col-sm-6 col-xs-12">
             <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
-         <a href="#">Scanfcode</a>.
+         <a href="#"> TO-GATHER</a>.
             </p>
           </div>
 
-          <div class="col-md-4 col-sm-6 col-xs-12">
+          <div class="col-md-5 col-sm-6 col-xs-12">
             <ul class="social-icons">
               <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
               <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top ">
             <div className="container-fluid">
-                <span className="navbar-brand mx-4"  href="#">Climex</span>
+                <span className="navbar-brand mx-4"  href="#">TO-GATHER</span>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
