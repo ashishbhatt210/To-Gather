9264b443010e3b580ec1faab9c6de4b2cea2fca0
Godflamejava <godflamestarlord@gmail.com>
2022-09-25 22:05:03 +0530

    login

--- src/components/loginSignup/LoginSignup.js
+++ src/components/loginSignup/LoginSignup.js
@@ -1,8 +1,50 @@
 import React, { useState, useEffect } from "react";
+import {Routes, Route, useNavigate} from 'react-router-dom';
 import "./loginFunction.js"
 import "./login.css"
 
 const LoginSignUp = () => {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password,setPassword]=useState("");
+    const changeDetail =(event)=>
+    {
+        const nametype=event.target.name;
+        const value=event.target.value;
+        if(nametype === "email")
+        {
+            setEmail(value);
+        }
+        else if(nametype === "password"){
+            setPassword(value);
+        }
+       
+    }
+    const login =(event)=>
+    {
+        fetch(`https://climex-5bd2f-default-rtdb.firebaseio.com/users.json`)
+        .then(results => results.json())
+        .then(data => {
+            for (let i = 0; i < data.length; i++) {
+                console.log(data[i].email+" "+email);
+                console.log(data[i].password+" "+password);
+
+                if(data[i].email==email&&data[i].password==password)
+                {
+                localStorage.setItem('name',data[i].name);
+                localStorage.setItem('email',data[i].email);
+                localStorage.setItem('password',data[i].password);
+                alert("Welcome back");
+                navigate('/');
+                return;
+                }
+            }
+            alert("Wrong Credentials");
+        })
+}
+       
+    
+
     return (
         <div className="outerlogin-container d-flex align-item-center justify-content-center">
             <div className="container-1" id="container-1">
@@ -34,10 +76,10 @@ const LoginSignUp = () => {
                             </ul>
                         </div>
                         <span className="signIn-span-tag">or use your account</span>
-                        <input className="signIn-input-tag" type="email" placeholder="Email" />
-                        <input className="signIn-input-tag" type="password" placeholder="Password" />
+                        <input className="signIn-input-tag" type="email" placeholder="Email" value={email} name={"email"}  onChange={changeDetail}/>
+                        <input className="signIn-input-tag" type="password" placeholder="Password" name={"password"} value={password} onChange={changeDetail} />
                         <a className="signIn-a-tag" href="#">Forgot your password?</a>
-                        <button className="signIn-button-tag">Sign In</button>
+                        <button className="signIn-button-tag" onClick={login}>Sign In</button>
                     </form>
                 </div>
                 <div className="overlay-container-1">
