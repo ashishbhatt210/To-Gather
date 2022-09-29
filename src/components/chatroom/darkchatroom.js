a835d4a8bcc0e6c1bbf476e2c2cd69b46c5f3484
Ashish Bhatt <bhattashish210@gmail.com>
2022-09-29 21:03:11 +0530

    Revert "final-change"
    This reverts commit 0e3c4bef69c3e4f29897001d32ef8457dc5be65f.

--- src/components/chatroom/darkchatroom.js
+++ src/components/chatroom/darkchatroom.js
@@ -15,25 +15,6 @@ console.log("ritik");
 
 const DarkChatRoom = () => {
 
-    window.onload = function(){
-        const toggleButton = document.querySelector('.dark-light');
-        const colors = document.querySelectorAll('.color');
-
-        colors.forEach(color => {
-        color.addEventListener('click', (e) => {
-            colors.forEach(c => c.classList.remove('selected'));
-            const theme = color.getAttribute('data-color');
-            document.body.setAttribute('data-theme', theme);
-            color.classList.add('selected');
-        });
-        });
-
-
-        toggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
-        });
-    }
-
     const sendMessage = async () => {
       if(message!=""){
         const tex=message.toString()
